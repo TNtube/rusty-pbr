fb42681b076082ec9e8f33b5062949c7a3c2355d
Stanis Kapusta <51389578+TNtube@users.noreply.github.com>
2023-12-10 23:45:25 +0100

    Basic window support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use gpu_allocator::d3d12::Allocator;
-
-
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder
+};
 fn main() {
-    println!("Hello, world!");
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    event_loop.run(move |event, elwt | {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                elwt.exit();
+            },
+            Event::AboutToWait => {
+                // gameloop
+                println!("each time ? Idk bruh");
+            }
+            _ => ()
+        }
+
+    }).expect("loop failed");
 }
